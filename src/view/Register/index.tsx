af69145d1e0286a01544efffce054cb7b3a24659
yacodev <cyaco33@gmail.com>
2024-04-08 09:04:02 -0500

    Add HeaderPage component

--- src/view/Register/index.tsx
+++ src/view/Register/index.tsx
@@ -1,6 +1,9 @@
+import { HeaderPage } from '../../components/HeaderPage';
+
 export const RegisterPage = () => {
   return (
     <div>
+      <HeaderPage />
       <h1>Register Page</h1>
     </div>
   );
