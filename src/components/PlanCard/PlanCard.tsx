7f48697764f414f4770482dc8d69756d0cd88728
yacodev <cyaco33@gmail.com>
2024-04-09 22:15:53 -0500

    show discounts in PlanCard

--- src/components/PlanCard/PlanCard.tsx
+++ src/components/PlanCard/PlanCard.tsx
@@ -1,50 +1,58 @@
-import { PlanList } from '../../models/planModel';
 import homeIcon from '../../assets/home-icon.svg';
 import hospitalIcon from '../../assets/hospital-icon.svg';
+import { PlanCardProps } from './types';
 import './planCard.scss';
 
-export interface PlanCardProps {
-  plans: PlanList[];
-  onSelectPlan: (index: number) => void;
-}
-
-export const PlanCard = ({ plans, onSelectPlan }: PlanCardProps) => {
+export const PlanCard = ({
+  plans,
+  onSelectPlan,
+  showDiscount = false,
+}: PlanCardProps) => {
   return (
-    <section className='plan-selection-container'>
+    <section className='plan'>
       {plans.map((plan, index) => {
         return (
-          index < 3 && (
-            <article key={index} className='plan-selection-container__card'>
-              <div
-                className='plan-selection-container__tag'
-                style={{ visibility: index !== 1 ? 'hidden' : 'visible' }}
-              >
-                Plan Recomendado
-              </div>
-              <div className='plan-selection-container__title'>
-                <span>{plan.name}</span>
-                <img
-                  src={plan.name.includes('Clínica') ? hospitalIcon : homeIcon}
-                  alt=''
-                />
-              </div>
+          <article key={index} className='plan__card'>
+            <div
+              className='plan__card--tag'
+              style={{ visibility: index !== 1 ? 'hidden' : 'visible' }}
+            >
+              Plan Recomendado
+            </div>
+            <div className='plan__card--title'>
+              <span>{plan.name}</span>
+              <img
+                src={plan.name.includes('Clínica') ? hospitalIcon : homeIcon}
+                alt='icono del plan'
+              />
+            </div>
 
-              <span>COSTO DEL PLAN</span>
-              <del>$99 antes</del>
-              <p>${plan.price} al mes</p>
-              <hr />
-              <div>
-                <ul className='plan-selection-container__description'>
-                  {plan.description.map((d, k) => {
-                    return <li key={k}>{d}</li>;
-                  })}
-                </ul>
-              </div>
-              <button onClick={() => onSelectPlan(index)}>
+            <span className='plan__card--plan-name'>COSTO DEL PLAN</span>
+            {showDiscount && (
+              <del className='plan__card--plan-discount'>
+                ${plan.price} antes
+              </del>
+            )}
+            <p className='plan__card--plan-price'>
+              ${showDiscount ? plan.price * 0.95 : plan.price} al mes
+            </p>
+            <hr />
+            <div className='plan__card--description-container'>
+              <ul className='plan__card--description'>
+                {plan.description.map((d, k) => {
+                  return <li key={k}>{d}</li>;
+                })}
+              </ul>
+            </div>
+            <div className='button__container'>
+              <button
+                className='button__plan'
+                onClick={() => onSelectPlan(index)}
+              >
                 Seleccionar Plan
               </button>
-            </article>
-          )
+            </div>
+          </article>
         );
       })}
     </section>
